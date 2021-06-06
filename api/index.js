f61da012f9a689a2bc4b1f8e09c54a556ba883af
christiangns <christian.santos@focustextil.com.br>
2021-06-05 22:55:55 -0300

    separando arquivos graphQL

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
-const { ApolloServer } = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
+const userSchema = require('./users/schema/user.graphql');
+
+const users  = [
+    { 
+        name: 'Ana',
+        active: true
+
+    },
+    { 
+        name: 'João',
+        active: false
+
+    }
+]
+
+const typeDefs = [ userSchema ];
+const resolvers = {}
 
 const server = new ApolloServer( { typeDefs, resolvers });
